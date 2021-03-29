aa94cc9a0dc486e6232dfa6d1568f29a4628a389
gmcdr <tharazo101@gmail.com>
2021-03-29 16:56:42 -0300

    Despertador C#

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Estudo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      double h, m, hd, md = 0;
+      
+      
+      Console.WriteLine("Digite a hora ex : [1,2,13,14..] : ");
+      h = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Digite os minutos ex : [21,30,40...] : ");
+      m = Convert.ToInt32(Console.ReadLine());
+     
+      
+      while (true)
+      {   
+          DateTime now = DateTime.Now;
+
+          hd = now.Hour;
+          md = now.Minute;
+            
+          if (h == hd & m == md)
+          {
+              break;
+          }
+      }     
+
+    }
+  }
+}
